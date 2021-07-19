b304b2fe4b8ff62574dbe079ccd472b943f2c955
ritheboy <ritheboy@ukr.net>
2021-07-19 09:35:55 +0300

    insert

--- MyVectorC++/MyVectorC++.cpp
+++ MyVectorC++/MyVectorC++.cpp
@@ -4,6 +4,28 @@
 
 class Vector
 {
+private:
+	void CheckArray()
+	{
+		if (!array)
+		{
+			array = new int[DEFAULT_ARRAY_SIZE];
+		}
+
+		if (size == capacity)
+		{
+			capacity *= CAPACITY_INCREASE;
+			int* newArray = new int[capacity];
+
+			for (int i = 0; i < size; i++)
+			{
+				newArray[i] = array[i];
+			}
+
+			delete[] array;
+			array = newArray;
+		}
+	}
 public:
 	const int DEFAULT_ARRAY_SIZE = 5;
 	const int CAPACITY_INCREASE = 2;
@@ -14,39 +36,70 @@ public:
 
 	void Push(int value)
 	{
-		if (!array)
-		{
-			array = new int[DEFAULT_ARRAY_SIZE];
-		}
-		if (size == capacity)
-		{
-			capacity = capacity * CAPACITY_INCREASE;
-			int* newArray = new int[capacity];
-			for (int i = 0; size > i; i++)
-			{
-				newArray[i] = array[i];
-			}
-			delete[] array;
-			array = newArray;		
-		}
+		CheckArray();
 		array[size++] = value;
+	}
+
+	void Insert(int value, int index)
+	{
+		if (index < 0)
+		{
+			return;
+		}
+		CheckArray();
+
+		if (index == size + 1 || index == 0 && size == 0)
+		{
+			Push(value);
+			return;
+		}
+
+		int current = size - 1;
+
+		if (index > size)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				CheckArray();
+				if (current + 1 == index)
+				{
+					array[current++] = value;
+				}
+				array[current++] = 0;
+				size++;
+			}
+			return;
+		}
+
+		for (int i = 0; i < size && index <= current; i++)
+		{
+			array[current + 1] = array[current];
+			current--;
+		}
+		size++;
+		array[index] = value;
 	}
 
 	int Find(int FindNumber)
 	{
-		for (int i = 0; size > i; i++)
+		for (int i = 0; i < size; i++)
 		{
 			if (array[i] == FindNumber)
 			{
 				return i;
 			}
 		}
-		return nullptr;
+		return -1;
+	}
+
+	int FindAt(int index)
+	{
+		return array[index];
 	}
 
 	void Print()
 	{
-		for (int i = 0; size > i; i++)
+		for (int i = 0; i < size; i++)
 		{
 			std::cout << array[i] << " ";
 		}
@@ -63,8 +116,6 @@ int main()
 	vector.Push(40);
 	vector.Push(50);
 	vector.Push(60);
-	vector.Push(70);
-
+	vector.Insert(5,8);
 	vector.Print();
-	std::cout << vector.Find(110);
 }
